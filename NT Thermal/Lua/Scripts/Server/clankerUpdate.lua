79add2f2f1e0323a8ce0254bba5203fddc8c50fc
BigEvilGreenBean <Redsandguy56@gmail.com>
2026-05-18 21:46:09 -0700

    Delete NT Thermal/Lua/Scripts/Server/clankerUpdate.lua

--- NT Thermal/Lua/Scripts/Server/clankerUpdate.lua
+++
@@ -1,5 +0,0 @@
--- Since the clankers are too full of themselves for flesh related temperature stuff.
--- This human update is purely for the clankers. Hence why it's called clankerUpdate.
-
-THERMClankers = {}
-NTTHERMRobot = {}
